5728985bfb7dd3030b3d7278cefecfac9110c7c2
Enes Usta <enesusta@email.com>
2020-09-20 14:45:17 +0300

    Implement useDocumentTitle

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export {default as useAuthFetch} from './http/useAuthFetch';
 export {default as useFetch} from './http/useFetch';
+export {default as useInput} from './form/useInput';
+export {default as useObject} from './form/useObject';
+export {default as useDocumentTitle} from './util/useDocumentTitle';
